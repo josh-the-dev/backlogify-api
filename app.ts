93329aabf470d30aca335063ba6df4b73493b01a
Joshua Grant <grantjoshua1995@gmail.com>
2020-03-15 17:33:05 +0000

    Starting to add routes in api

--- app.ts
+++ app.ts
@@ -1,10 +1,13 @@
-const path = require('path');
-const express = require('express');
+import path from 'path';
+import express from 'express';
+import backlogs from './modules/backlogs/backlogs.routes';
 const app = express();
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, './public');
 
 app.use(express.static(publicPath));
+
+app.use('/backlogs', backlogs);
 
 app.get('*', (_req: any, res: { sendFile: (arg0: any) => void }) => {
   res.sendFile(path.join(publicPath, 'index.html'));
