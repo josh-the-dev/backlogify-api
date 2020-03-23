01c3ef5f7e30aaa5961a85c66ba68becda8eb934
Joshua Grant <grantjoshua1995@gmail.com>
2020-03-23 22:11:34 +0000

    Setup Psql connection with env variables

--- config.ts
+++ config.ts
@@ -1,3 +1,8 @@
 export default {
   igdbApiKey: process.env.IGDB_API_KEY,
+  pgUsername: process.env.PG_USERNAME,
+  pgDatabase: process.env.PG_DATABASE,
+  pgPort: process.env.PG_PORT,
+  pgHost: process.env.PG_HOST,
+  pgPassword: process.env.PG_PASSWORD,
 };
