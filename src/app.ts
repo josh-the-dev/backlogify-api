1de6eb6d3fb1f97bf5b62d1a28fa46ab8801a387
Joshua Grant <grantjoshua1995@gmail.com>
2020-03-29 12:07:14 +0100

    Now have backlogs route talking to psql

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,24 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
-import { Pool, Client } from 'pg';
-import routes from './modules/routes';
+import { Client } from 'pg';
+
 import config from '../config';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-const client = new Client({
+export const client = new Client({
   user: config.pgUsername,
   host: config.pgHost,
   password: config.pgPassword,
   database: config.pgDatabase,
   port: 5432,
 });
-client.connect();
+import routes from './modules/routes';
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use('/api/backlogs', routes.backlogs);
 app.use('/api/igdb', routes.igdb);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
-  client.query('SELECT * FROM backlogs', (err, res) => {
-    console.log(res);
-  });
 });
--- src/modules/backlogs/backlogs.routes.ts
+++ src/modules/backlogs/backlogs.routes.ts
@@ -1,5 +1,22 @@
 import express from 'express';
+import { client } from '../../app';
 
 const routes = express.Router();
+
+client.connect();
+
+routes.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  client.query(`SELECT * FROM backlogs where id = ${id}`, (err, results) => {
+    if (err) {
+      throw err;
+    }
+    if (results.rowCount === 0) {
+      return res.sendStatus(404);
+    } else {
+      return res.status(200).json(results.rows);
+    }
+  });
+});
 
 export default routes;
