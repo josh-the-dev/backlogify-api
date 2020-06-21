2dd9ab5f11f40bb8dde8382a8b87af3bfb5e6b70
Joshua Grant <grantjoshua1995@gmail.com>
2020-06-21 13:05:35 +0100

    Fixing user backlogs route to return all backlogs + games correctly

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { client } from '../../app';
-import { User } from 'src/types';
+import { User, Backlog, BacklogGame } from 'src/types';
 
 const routes = express.Router();
 
@@ -24,19 +24,38 @@ routes.post('/register', async (req, res) => {
 
 routes.get('/:id/backlogs', async (req, res) => {
   const { id } = req.params;
-  client.query(
-    `SELECT id from backlogs WHERE backlogs.user_id = ${id}`,
-    (err, results) => {
-      if (err) {
-        throw err;
-      }
-      if (results.rowCount === 0) {
-        return res.sendStatus(404);
-      } else {
-        return res.status(200).json(results.rows);
-      }
-    }
+  const backlogsResults = await client.query(
+    `SELECT id, name, backlog_order from backlogs WHERE backlogs.user_id = ${id}`
   );
+  if (backlogsResults.rowCount === 0) {
+    return res.sendStatus(404);
+  } else {
+    let userBacklogs: Backlog[] = backlogsResults.rows;
+    let backlogGames: BacklogGame[] = [];
+    userBacklogs.map(backlog => {
+      backlog.games = [];
+    });
+
+    const getUserBacklogGames = async () => {
+      const userBacklogPromises = userBacklogs.map(async backlog => {
+        const backlogGamesResult = await client.query(
+          `SELECT * FROM backlog_games where backlog_id = ${backlog.id}`
+        );
+        backlogGames = backlogGamesResult.rows;
+      });
+      await Promise.all(userBacklogPromises);
+    };
+
+    await getUserBacklogGames();
+    backlogGames.forEach(backlogGame => {
+      const relatedBacklogIndex = userBacklogs.findIndex(
+        backlog => backlog.id === backlogGame.backlog_id
+      );
+      userBacklogs[relatedBacklogIndex].games.push(backlogGame);
+    });
+
+    return res.status(200).json(userBacklogs);
+  }
 });
 
 export default routes;
