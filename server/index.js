a5197ac6825e31ca4172f81fab9663c5d8011796
Joshua Grant <grantjoshua1995@gmail.com>
2021-06-19 21:42:39 +0200

    Adding ES6 support to node and fixing build

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
