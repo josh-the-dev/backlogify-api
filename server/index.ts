737afd2384b9d93330443ab6449b125a09f1fab0
Joshua Grant <grantjoshua1995@gmail.com>
2021-06-20 23:49:57 +0200

    Adding typescript to backend

--- server/index.ts
+++ server/index.ts
@@ -5,10 +5,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const __dirname = path.resolve();
-
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
@@ -17,7 +15,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, ".//client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
